fc16ce432de91b0658d3206df3c94048046c2ccc
i-dunno-dis <sofiacieplak@gmail.com>
2026-03-18 09:29:22 +0000

    added level one to new file

---
+++ FatePrologue/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void ChangeToScene(string targetSceneName)
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
