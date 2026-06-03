156804e68a42769af6ef1efb51b7e3bd3003fdbe
i-dunno-dis <sofiacieplak@gmail.com>
2026-06-03 15:29:15 +0100

    updated comments on enemy patrol script

--- FatePrologue/Assets/Scripts/EnemyPatrol.cs
+++ FatePrologue/Assets/Scripts/EnemyPatrol.cs
@@ -18,32 +18,34 @@ public class EnemyPatrol : MonoBehaviour
         mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    
-
-
     // Update is called once per frame
     void Update()
     {
+        //this checks where the enemy is in accodance to the current patol point
         float distance = (patrolPoints[currentPoint] - (Vector2)transform.position).magnitude;
 
+        //this checks if the currnt patrol point has been reached
         if (distance <= stopDistance)
         {
             currentPoint = currentPoint + 1;
-
+            //this checks if the next patrol point was reached
             if (currentPoint >= patrolPoints.Length)
             {
+                //this sets the current patrol point to 0
                 currentPoint = 0;
             }
         }
-
+        //this sets the direction the enemy moves according to its current location 
         Vector2 direction = (patrolPoints[currentPoint] - (Vector2)transform.position).normalized;
+        //this sets the speed of the enemy
         ourRigidbody.linearVelocity = direction * forceStrength;
 
+        //this flips the sprite to go towards the negative x
         if (ourRigidbody.linearVelocityX < 0f)
         {
             mySpriteRenderer.flipX = true;
         }
-
+        //this flips the sprite to go towards the postive x
         else
         {
             mySpriteRenderer.flipX = false;
